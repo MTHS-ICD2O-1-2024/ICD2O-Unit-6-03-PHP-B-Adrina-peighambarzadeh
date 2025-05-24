a7f4c52695aa3cf130f57c97d6b41866d42db94d
Adrina-peighambarzadeh <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-24 14:06:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,64 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit6-03-PHP-MDL -->
+<!-- ICS2O-Unit6-03-PHP -->
 <html lang="en-ca">
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="API & JSON weather app" />
+  <meta name="description" content="Current Weather Web Page PHP" />
   <meta name="keywords" content="mths, icd2o" />
-  <meta name="author" content="Adrina. peighambarzadeh" />
+  <meta name="author" content="Adrina peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
-  <link rel="manifest" href="site.webmanifest" />
-  <title>API & JSON weather app</title>
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.light_blue-orange.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Weather today, with API & JSON</title>
 </head>
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script src="./js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">API & JSON weather app</span>
+        <span class="mdl-layout-title">Weather today:</span>
       </div>
     </header>
     <main class="mdl-layout__content">
-      <div class="right-image">
+      <div class="page-content">Click the button to get the current weather.
+        <br />
+        <br />
+        <form action="#">
+          <!-- Accent-colored raised button with ripple -->
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+            type="submit">
+            Click to see the weather!
+          </button>
+        </form>
       </div>
-      <form action="./answer.php" method="GET">
-      <div class="page-content">Click the button bellow to calculate the current temperature</div>
-      <br />
-      <!-- Accent-colored raised button with ripple -->
-      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-        type="submit">
-        Click here!
-      </button>
-      </form>
-      <br />
-      <div class="page-content-answer">
-        <div id="answer"></div>
-      </div>
+      <?php
+      async () {
+        try {
+          $resultJSON = await fetch(
+            "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+          )
+          $jsonData = await resultJSON.json();
+          console.log(jsonData);
+          $weatherIconId = jsonData.weather[0].icon;
+          $weatherIconUrl = "https://openweathermap.org/img/wn/" + weatherIconId + "@2x.png";
+          $currentWeatherKelvin = jsonData.main.temp
+          $currentWeatherCelcius = currentWeatherKelvin - 273.15;
+      
+          // output
+          echo "<p> Temperature is " + $currentWeatherCelcius.toFixed(0) + "°C. </p> ". </br> <img src =" + $weatherIconUrl + " alt='Weather Icon'>"
+        } catch (error) {
+          // If an error has occured
+          echo "Sorry, an error has occured. Please try again later."
+        }
+      }
+      ?>
     </main>
   </div>
 </body>
